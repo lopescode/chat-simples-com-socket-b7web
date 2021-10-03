6ca71ea17c52df2566d8fe09a34dc85a47e68708
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-10-03 17:52:07 -0300

    :zap: Estabelecendo comunicação através do Socket

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
+const socketIO = require("socket.io");
+
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app = express();
 const server = require("http").createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "public")))
 
 server.listen(port)
 console.log(`Servidor rodando na porta ${port}`);
+
+io.on('connection', (socket) => {
+    console.log("Conexão estabelecida...");
+    
+})
