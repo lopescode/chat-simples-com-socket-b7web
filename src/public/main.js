176f565004624abb7d49f3a5ee2cd62314be2d2e
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-10-03 17:58:31 -0300

    :zap: Entrando no chat 1/2

--- src/public/main.js
+++ src/public/main.js
@@ -1,1 +1,22 @@
 const socket = io();
+
+let username = ""; 
+let userList = [];
+let loginPage = document.querySelector("#loginPage");
+let chatPage = document.querySelector("#chatPage");
+let loginNameInput = document.querySelector("#loginNameInput");
+let chatTextInput = document.querySelector("#chatTextInput");
+
+loginPage.style.display = "flex";
+chatPage.style.display = "none";
+
+loginNameInput.addEventListener("keyup", (e) => {
+    if(e.keyCode === 13) {
+        let name = loginNameInput.value.trim();
+        
+        if (name != "") {
+            username = name;
+            document.title = `Chat (${username})`;
+        }
+    }
+})
